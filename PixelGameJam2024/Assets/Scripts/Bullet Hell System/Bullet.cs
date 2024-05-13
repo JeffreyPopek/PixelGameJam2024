bf98206a3d373bedd1eae7e6077f532403d4f422
Jeffrey <jeffrey.popek@mymail.champlain.edu>
2024-05-12 17:29:38 -0700

    Bullet Boundaries
    Walls that will deactivate bullets when hit. Bullets effected by gravity for now

--- PixelGameJam2024/Assets/Scripts/Bullet Hell System/Bullet.cs
+++ PixelGameJam2024/Assets/Scripts/Bullet Hell System/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float lifetime = 3f;
+    [SerializeField] private float lifetime = 100f;
     [SerializeField] private float speed = 1f;
     [SerializeField] private float rotation = 0f;
 
@@ -12,7 +12,6 @@ public class Bullet : MonoBehaviour
     
     private void Start()
     {
-        
     }
 
     private void Update()
@@ -23,5 +22,25 @@ public class Bullet : MonoBehaviour
             Debug.Log("Bullet lifetime expired");
         }
         timer += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            gameObject.SetActive(false);
+            Debug.Log("Hit Wall");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Hit Wall");
+
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            gameObject.SetActive(false);
+            Debug.Log("Destroying Bullet");
+        }
     }
 }
