e47a7c87041b0c7752701a734d42454f8425ac62
Jeffrey <jeffrey.popek@mymail.champlain.edu>
2024-05-14 14:42:57 -0700

    Small changes to bullet

--- PixelGameJam2024/Assets/Scripts/Bullet Hell System/Bullet.cs
+++ PixelGameJam2024/Assets/Scripts/Bullet Hell System/Bullet.cs
@@ -10,10 +10,11 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float strength = 2f;
 
     private float timer = 0;
-    
+    private Vector2 spawnPoint;
+
     private void Start()
     {
-        
+        spawnPoint = new Vector2(transform.position.x, transform.position.y);
     }
 
     private void Update()
@@ -23,10 +24,19 @@ public class Bullet : MonoBehaviour
         if (timer > lifetime)
         {
             gameObject.SetActive(false);
+            //transform.position = Movement(timer);
             Debug.Log("Bullet lifetime expired");
         }
         timer += Time.deltaTime;
     }
+    
+    // private Vector2 Movement(float timer) {
+    //     // Moves right according to the bullet's rotation
+    //     float x = timer * speed * transform.right.x;
+    //     float y = timer * speed * transform.right.y;
+    //     return new Vector2(x+spawnPoint.x, y+spawnPoint.y);
+    // }
+
 
     private void OnCollisionEnter2D(Collision2D col)
     {
